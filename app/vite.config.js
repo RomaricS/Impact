87d1a600d57a9f9d7105a9355ddfb447fe491660
Romaric SEGLA <romaricboss@gmail.com>
2026-04-22 20:27:21 -0400

    fixes

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: new URL('.', import.meta.url).pathname,
   plugins: [react()],
 })
